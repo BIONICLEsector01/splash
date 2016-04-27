1b9a7363868f0a5b608ff6061aff5f65a41aab0e
George Barnick <georgebarnick@gmail.com>
2016-04-27 17:58:33 -0400

    Moving CSS into styles.css

--- index.php
+++ index.php
@@ -1,115 +1,32 @@
 <html>
-<head>
-<link rel="shortcut icon" href="http://biosector01.com/favicon.ico" />
-<title>BIONICLEsector01</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="theme-color" content="#0797B0">
-<style type="text/css">
-@font-face {
-        font-family: 'GoudyTrajan';
-        src: url('http://biosetor01.com/resources/goudytrajan-webfont.eot');
-        src: url('http://biosector01.com/resources/goudytrajan-webfont.eot?#iefix') format('embedded-opentype'),
-        url('http://biosector01.com/resources/goudytrajan-webfont.woff') format('woff'),
-        url('http://biosector01.com/resources/GoudyTrajan.ttf') format('truetype'),
-        url('http://biosector01.com/resources/goudytrajan-webfont.svg#goudytrajanregular') format('svg');
-        font-style: normal;
-}
-
-* {
-        padding: 0;
-        margin: 0;
-}
-
-body {
-        background: #a8dae3 url('http://biosector01.com/wiki/skins/MonoBook/bgbio2015.jpg') no-repeat center center fixed !important;
-        background-attachment: fixed !important;
-        background-size: cover !important;
-        text-align: center;
-        height: 100%;
-}
-
-input[type="text"] {
-        width: 500px;
-        background-color: #f9f9f9;
-        border: 1px solid #eaeaea;
-        border-radius: 4px;
-        padding: 11px 10px 9px;
-        font-size: 1.5em;
-        font-family: "GoudyTrajan", serif;
-}
-
-input[type="submit"] {
-        background-image: url('https://www.harrellsecurities.com/owncloud/core/img/actions/confirm.svg');
-        background-repeat: no-repeat;
-        background-position: center center;
-        background-color: transparent;
-        opacity: .3;
-        width: 40px;
-        height: 40px;
-        border: 0;
-        position: relative;
-        top: -43px;
-        left: 230px;
-        cursor: pointer;
-}
-
-input[type="submit"]:hover {
-        opacity: .7;
-}
-
-/* Desktop only */
-@media screen and (min-device-width: 480px) {
-        #content {
-                position: relative;
-                top: 40%;
-                transform: translateY(-60%);
-                -webkit-transform: translateY(-60%);
-                -ms-transform: translateY(-60%);
-        }
-}
-
-/* Mobile only */
-@media screen and (max-device-width: 480px) {
-        #content {
-                position: relative;
-                top: 2em;
-        }
-
-        img {
-                max-width: 300px;
-        }
-
-        input[type="text"] {
-                width: 400px;
-        }
-
-        input[type="submit"] {
-                left: 180px;
-        }
-}
-</style>
-</head>
-<body>
-<div id="content">
-<a href="/wiki/index.php/Main_Page">
-<img src="http://biosector01.com/images/FinalLogoNewHR.png" width="400" alt="BS01 Logo" />
-</a>
-<form action="search.php" method="get">
-<input type="text" name="query" placeholder="Search" value autofocus><br />
-<input type="submit" value>
-<input type="hidden" name="search" value="1">
-</form>
-</div>
-<!-- Start Quantcast tag -->
-<script type="text/javascript">/*<![CDATA[*/
-_qoptions = {
-        qacct: "p-PKr5qPjMC77bB"
-};
-/*]]>*/</script>
-<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
-<noscript>
-<img src="http://pixel.quantserve.com/pixel/p-PKr5qPjMC77bB.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast" />
-</noscript>
-<!-- End Quantcast tag -->
-</body>
+	<head>
+		<link rel="shortcut icon" href="http://biosector01.com/favicon.ico" />
+		<title>BIONICLEsector01</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="theme-color" content="#0797B0">
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+		<div id="content">
+			<a href="/wiki/index.php/Main_Page">
+				<img src="http://biosector01.com/images/FinalLogoNewHR.png" width="400" alt="BS01 Logo" />
+			</a>
+			<form action="search.php" method="get">
+				<input type="text" name="query" placeholder="Search" value autofocus><br />
+				<input type="submit" value>
+				<input type="hidden" name="search" value="1">
+			</form>
+		</div>
+		<!-- Start Quantcast tag -->
+		<script type="text/javascript">/*<![CDATA[*/
+			_qoptions = {
+			        qacct: "p-PKr5qPjMC77bB"
+			};
+		/*]]>*/</script>
+		<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
+		<noscript>
+			<img src="http://pixel.quantserve.com/pixel/p-PKr5qPjMC77bB.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast" />
+		</noscript>
+		<!-- End Quantcast tag -->
+	</body>
 </html>

93f06fcb4353ed7b2cf3438eae92c305568b417d
George Barnick <georgebarnick@gmail.com>
2016-04-27 17:44:53 -0400

    Add Quantcast tracking

--- index.php
+++ index.php
@@ -100,5 +100,16 @@ input[type="submit"]:hover {
 <input type="hidden" name="search" value="1">
 </form>
 </div>
+<!-- Start Quantcast tag -->
+<script type="text/javascript">/*<![CDATA[*/
+_qoptions = {
+        qacct: "p-PKr5qPjMC77bB"
+};
+/*]]>*/</script>
+<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
+<noscript>
+<img src="http://pixel.quantserve.com/pixel/p-PKr5qPjMC77bB.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast" />
+</noscript>
+<!-- End Quantcast tag -->
 </body>
 </html>
